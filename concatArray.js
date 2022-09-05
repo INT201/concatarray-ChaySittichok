74272901c26efe093c0984e35983990c53c23df8
ChaySittichok <sittichok.3345@mail.kmutt.ac.th>
2022-09-05 15:01:15 +0700

    update

--- concatArray.js
+++ concatArray.js
@@ -2,5 +2,18 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
+if((array1 == null || undefined) && (array2 == null || undefined)){
+  return undefined
+}
+else if(array1 == null || undefined ){
+  return array2
+}
+else if(array2 == null || undefined ){
+  return array1
+}
+else if((array1 == [])&&(array2 == [])){
+  return '[]'
+}
+
 }
 module.exports = concatArray
